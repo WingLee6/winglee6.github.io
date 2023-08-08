cfb87260b3dc30f1028ae45eb1cd3cc095e00e42
Luke Lee <luke_lee_98@163.com>
2023-08-08 10:47:47 +0800

    update 0808

--- js/CircleRobot.js
+++ js/CircleRobot.js
@@ -28,13 +28,6 @@ CircleRobot.prototype.IsAvailable = function (nAvailableNum) {
 CircleRobot.prototype.GetState = function () {
     console.log("START CircleRobot.GetState")
     
-    if (this.pos[0].toString() == this.pos[1].toString() & this.pos[1].toString() == this.pos[2].toString()) {
-        var fCirclePosX = this.pos[0][0]
-        var fCirclePosY = this.pos[0][1]
-        this.pos.splice(0, this.pos.length)
-        return '点圆, 圆的半径 r = 0px, 圆心坐标[' + fCirclePosX.toFixed(3) + ', ' + fCirclePosY.toFixed(3) + '], 圆的标准方程为 (x - ' + fCirclePosX + ')^2 + (y -' + fCirclePosY + ')^2 = 0'
-    }
-
     // 结合圆的一般方程 x^2 + y^2 + ax + by + c = 0 带入三点求abc
     var strEquation1 = this.pos[0][0] ** 2 + this.pos[0][1] ** 2 + '+' + this.pos[0][0] + '*a+' + this.pos[0][1] + '*b+c=0'
     var strEquation2 = this.pos[1][0] ** 2 + this.pos[1][1] ** 2 + '+' + this.pos[1][0] + '*a+' + this.pos[1][1] + '*b+c=0'
@@ -49,6 +42,9 @@ CircleRobot.prototype.GetState = function () {
         var fCirclePosY = -0.5 * solutionList[1][1]
         return '圆的半径 r = ' + fCilcleR.toFixed(3) + 'px, 圆心坐标[' + fCirclePosX.toFixed(3) + ', ' + fCirclePosY.toFixed(3) + '], 圆的一般式为 x^2 + y^2 + ' + solutionList[0][1].toFixed(3) + 'x + ' + solutionList[1][1].toFixed(3) + 'y + ' + solutionList[2][1].toFixed(3) + ' = 0'
     } catch(err) {
+        if (this.pos[0].toString() == this.pos[1].toString() & this.pos[1].toString() == this.pos[2].toString()) {
+            return '点圆, 圆的半径 r = 0px, 圆心坐标[' + this.pos[0][0] + ', ' + this.pos[0][1] + '], 圆的标准方程为 (x - ' + this.pos[0][0] + ')^2 + (y -' + this.pos[0][1] + ')^2 = 0'
+        }
         return '虚圆, 输入的三点分别为' + '[' + this.pos[0] + '], ' + '[' + this.pos[1] + '], ' + '[' + this.pos[2] + ']'
     } finally {
         this.pos.splice(0, this.pos.length)
